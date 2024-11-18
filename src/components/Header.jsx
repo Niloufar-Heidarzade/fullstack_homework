4bfab49dbb99b54762b59ea0da2ba0665ac16626
Niloufar Heidarzade <Niiloufar.Heidarzadeh@gmail.com>
2024-11-18 18:14:18 +0330

    header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,10 +8,18 @@ import ShowInCards from "./ShowInCards";
 import SortBy from "./SortBy";
 import { useSelector , useDispatch} from "react-redux";
 import {controlSidebar} from "../redux/sidebarSlice.js";
+import { useEffect , useState} from "react";
 
 function Header() {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
   const isSidebarOpen = useSelector((state) => state.sidebar.isSidebarOpen)
   const dispatch = useDispatch();
+  useEffect(() => {
+    const handleResize = () => setScreenWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   function openSidebar() {
     dispatch(controlSidebar())
   }
@@ -22,11 +30,12 @@ function Header() {
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="rgb(40 , 40 , 40)" class="w-6 h-6" className="menu-icon" onClick={() => openSidebar()}>
           <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
           </svg>
-          <SearchBar />
+          {screenWidth > 560 && <SearchBar size={"small"}/>}
         </div>
         <TodayDate />
         <AddNewTaskButton size="small" />
       </div>
+      {screenWidth < 560 && <SearchBar size={"big"}/>}
       <TasksNumber className="number-of-tasks"/>
       <div className="header-second-line">
         <div className="header-icons">
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,8 @@
 import "../styles/search-bar.css";
 
-function SearchBar() {
+function SearchBar({size}) {
   return(
-    <div className="search-task-bar" tabindex="0">
+    <div className={size === "small" ? "search-task-bar" : "search-task-bar big-search-task-bar"} tabindex="0">
       <input type="text" placeholder="Search task" className="search-task-input"/>
       <img src="./images/search.svg" className="search-task-svg"/>
     </div>
